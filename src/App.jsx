99e7d47ceaa682fffb97adfef01fac68d9ce5835
Sudhanshu Tiwari <sudhanshutiwari9836@gmail.com>
2024-05-28 09:32:28 +0530

    Made all pages and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
+import { Router } from "react-router-dom"
 import Topbar from "./components/upbar/upbar"
 import Home from "./pages/home/home"
+import Login from "./pages/login/login"
+import Register from "./pages/register/register"
+import Settings from "./pages/settings/settings"
 import Single from "./pages/single/single"
 import Write from "./pages/write/write"
 function App() {
 
   return (
-    <>
+    <Router>
      <Topbar/>
-     {/* <Home/> */}
-     {/* <Single/> */}
-     <Write/>
-    </>
+     <Register/>
+    </Router>
   )
 }
 
--- src/pages/write/write.jsx
+++ src/pages/write/write.jsx
@@ -3,6 +3,8 @@ import "./write.css"
 function Write() {
   return (
     <div className="write">
+      <img className="writeImg" alt="write-img" 
+      src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"/>
       <form className="writeForm">
         <div className="writeFormGroup">
             <label htmlFor="fileInput">
